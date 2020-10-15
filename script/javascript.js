510c7899920ca65d8a2788b7f3d2b4a00556cf8d
Giulia <giulia.guerrazzi@live.it>
2020-10-15 19:42:42 +0200

    revision

--- script/javascript.js
+++ script/javascript.js
@@ -4,19 +4,20 @@ var nome = prompt('Inserisci il tuo nome');
 document.getElementById('name').innerHTML = 'Salve ' + nome;
 
 // age
-var age = prompt('Quanti anni hai?');
+var age = parseInt (prompt('Quanti anni hai?') );
 
 document.getElementById('age').innerHTML = age;
 
 // km
-var distance = prompt('Quanti km devi percorrere?');
+var distance = parseInt (prompt('Quanti km devi percorrere?') );
 
 document.getElementById('km').innerHTML = distance;
 
 // ticket
 var priceTicket = distance * 0.21;
+var n = priceTicket.toFixed(2);
 
-document.getElementById('priceTicket').innerHTML = priceTicket;
+document.getElementById('priceTicket').innerHTML = priceTicket.toFixed(2);
 
 var sale20 = (priceTicket / 100) * 20;
 var sale40 = (priceTicket / 100) * 40;
@@ -24,12 +25,14 @@ var sale40 = (priceTicket / 100) * 40;
 // sale
 if (age <= 18) {
   var finalTicket18 = priceTicket - sale20;
-  document.getElementById('sconto20').innerHTML = 'su cui viene applicato uno sconto del 20%';
-  document.getElementById('finalTicket18').innerHTML = finalTicket18;
+  var n = finalTicket18.toFixed(2);
+  document.getElementById('sconto20').innerHTML = 'su cui viene applicato uno sconto del 20% &#8364;';
+  document.getElementById('finalTicket18').innerHTML = finalTicket18.toFixed(2);
 }
 
 if (age >= 65) {
   var finalTicket65 = priceTicket - sale40;
-  document.getElementById('sconto40').innerHTML = 'su cui viene applicato uno sconto del 40%';
-  document.getElementById('finalTicket65').innerHTML = finalTicket65;
+  var n = finalTicket65.toFixed(2);
+  document.getElementById('sconto40').innerHTML = 'su cui viene applicato uno sconto del 40% &#8364;';
+  document.getElementById('finalTicket65').innerHTML = finalTicket65.toFixed(2);
 }
